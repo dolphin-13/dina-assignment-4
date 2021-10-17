654f05ba5dc3411169d510fbdffcf04954f64b67
dolphin-13 <55006453+dolphin-13@users.noreply.github.com>
2021-10-17 00:59:38 -0700

    Fixed sorting bug in Assignment4

--- src/com/codercampus/FileService.java
+++ src/com/codercampus/FileService.java
@@ -6,7 +6,6 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Arrays;
-import java.util.Comparator;
 
 public class FileService {
 	static String line;
@@ -38,34 +37,17 @@ public class FileService {
 
 			writer = new BufferedWriter(new FileWriter(file));
 			writer.write("Student ID,Student Name,Course,Grade\n");
-			for (Student student : students) {
-				if (student.getCourse().contains(course)) {
-					
-					Arrays.sort(students, new Comparator<Student>() {
-
-						@Override
-						public int compare(Student student1, Student student2) {
-							if (student1.getGrade() == null && student2.getGrade() == null) {
-								return 0;
-							} else if (student1.getGrade() == null) {
-								return 1;
-							} else if (student2.getGrade() == null) {
-								return -1;
-							}
-							return student2.getGrade().compareTo(student1.getGrade());
-						}
-					});
-					writer.write(student.getStudentID() + "," + student.getStudentName() + "," + student.getCourse()
-							+ "," + student.getGrade() + "\n");
+			for (int i = 1; i < students.length; i++) {
+				if (students[i].getCourse().contains(course)) {
+					Arrays.sort(students, new StudentGradeComparator());
+					writer.append(students[i].getStudentID() + "," + students[i].getStudentName() + ","
+							+ students[i].getCourse() + "," + students[i].getGrade() + "\n");
 				}
-
 			}
 
 		} finally {
 			if (writer != null)
 				writer.close();
 		}
-
 	}
-
 }
